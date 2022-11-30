b8d6583dda3f3fcb901aa398b4fdec809f7593ae
vithaln <vnnivargi1998@gmail.com>
2022-11-30 21:29:18 +0530

    removed related FE

--- blog-app-apis/src/main/java/com/vithal/blog/config/SecurityConfig.java
+++ blog-app-apis/src/main/java/com/vithal/blog/config/SecurityConfig.java
@@ -144,32 +144,5 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
-
-
-    @Bean
-    public FilterRegistrationBean coresFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOriginPattern("*");
-        corsConfiguration.addAllowedHeader("Authorization");
-        corsConfiguration.addAllowedHeader("Content-Type");
-        corsConfiguration.addAllowedHeader("Accept");
-        corsConfiguration.addAllowedMethod("POST");
-        corsConfiguration.addAllowedMethod("GET");
-        corsConfiguration.addAllowedMethod("DELETE");
-        corsConfiguration.addAllowedMethod("PUT");
-        corsConfiguration.addAllowedMethod("OPTIONS");
-        corsConfiguration.setMaxAge(3600L);
-
-        source.registerCorsConfiguration("/**", corsConfiguration);
-
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-
-        bean.setOrder(-110);
-
-        return bean;
-    }
-
+	
 }
