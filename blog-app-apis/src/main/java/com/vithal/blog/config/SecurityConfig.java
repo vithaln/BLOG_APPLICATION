da805afa2535379d3e75d4a91c1333e1c5aed1ce
vithaln <vnnivargi1998@gmail.com>
2023-02-20 19:22:54 +0530

    CORS configuration done for the UI(ReactJS).

--- blog-app-apis/src/main/java/com/vithal/blog/config/SecurityConfig.java
+++ blog-app-apis/src/main/java/com/vithal/blog/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vithal.blog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.vithal.blog.security.CustomUserDetailService;
@@ -28,116 +32,123 @@ import com.vithal.blog.security.JwtAuthenticationFilter;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    public static final String[] PUBLIC_URLS = {"/api/v1/auth/**", "/v3/api-docs", "/v2/api-docs",
-            "/swagger-resources/**", "/swagger-ui/**", "/webjars/**"
+	public static final String[] PUBLIC_URLS = { "/api/v1/auth/**", "/v3/api-docs", "/v2/api-docs",
+			"/swagger-resources/**", "/swagger-ui/**", "/webjars/**"
 
-    };
+	};
 
-    @Autowired
-    private CustomUserDetailService customUserDetailService;
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
 
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().requestMatchers(PUBLIC_URLS).permitAll()
+				.requestMatchers(HttpMethod.GET).permitAll()
 
-        http.
-                csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(PUBLIC_URLS)
-                .permitAll()
-                .requestMatchers(HttpMethod.GET)
-                .permitAll()
-               
-                .anyRequest()
-                .authenticated()
-                .and().exceptionHandling()
-                .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        http.authenticationProvider(daoAuthenticationProvider());
-        DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
-
-        return defaultSecurityFilterChain;
-
-
-    }
-
-	/*
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.
-				csrf()
-				.disable()
-				.authorizeHttpRequests()
-				.antMatchers(PUBLIC_URLS)
-				.permitAll()
-				.antMatchers(HttpMethod.GET)
-				.permitAll()
-				.anyRequest()
-				.authenticated()
-				.and().exceptionHandling()
-				.authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
-				.and()
-				.sessionManagement()
+				.anyRequest().authenticated().and().exceptionHandling()
+				.authenticationEntryPoint(this.jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+		http.authenticationProvider(daoAuthenticationProvider());
+		DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+
+		return defaultSecurityFilterChain;
+
 	}
-
-	 */
-
-    /*
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
-
-    }
-
-    */
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 	/*
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * 
+	 * http. csrf() .disable() .authorizeHttpRequests() .antMatchers(PUBLIC_URLS)
+	 * .permitAll() .antMatchers(HttpMethod.GET) .permitAll() .anyRequest()
+	 * .authenticated() .and().exceptionHandling()
+	 * .authenticationEntryPoint(this.jwtAuthenticationEntryPoint) .and()
+	 * .sessionManagement() .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	 * 
+	 * http.addFilterBefore(this.jwtAuthenticationFilter,
+	 * UsernamePasswordAuthenticationFilter.class);
+	 * 
+	 * }
+	 * 
 	 */
 
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * 
+	 * auth.userDetailsService(this.customUserDetailService).passwordEncoder(
+	 * passwordEncoder());
+	 * 
+	 * }
+	 * 
+	 */
 
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(this.customUserDetailService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    }
+	/*
+	 * @Bean
+	 * 
+	 * @Override public AuthenticationManager authenticationManagerBean() throws
+	 * Exception { return super.authenticationManagerBean(); }
+	 * 
+	 */
+
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(this.customUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+
+	}
 
 //this bean used in controller layer.
-    @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-	
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	/*
+	 * for FrontEnd connection we need use @Crossrigin if we want to use @Crossrigin
+	 * we should use each controller layers... so we have alternate way for this
+	 * implementations as below implemented .... looks it's global .
+	 */
+	@Bean
+	public FilterRegistrationBean coresFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.addAllowedOriginPattern("*");
+		corsConfiguration.addAllowedHeader("Authorization");
+		corsConfiguration.addAllowedHeader("Content-Type");
+		corsConfiguration.addAllowedHeader("Accept");
+		corsConfiguration.addAllowedMethod("POST");
+		corsConfiguration.addAllowedMethod("GET");
+		corsConfiguration.addAllowedMethod("DELETE");
+		corsConfiguration.addAllowedMethod("PUT");
+		corsConfiguration.addAllowedMethod("OPTIONS");
+		corsConfiguration.setMaxAge(3600L);
+
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+		bean.setOrder(-110);
+
+		return bean;
+	}
 }
